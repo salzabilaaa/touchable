80bfcc86ccee30e77c456e47e1b9ffe7c12ffad3
unknown <salsabilasusan20@gmail.com>
2022-12-07 04:44:48 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+ 
+
+const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
+ 
+
+  return (
+
+    <View style={styles.container}>
+
+      <View style={styles.countContainer}>
+
+        <Text>Angka: {count}</Text>
+
+      </View>
+
+      <TouchableOpacity
+
+        style={styles.button}
+
+        onPress={onPress}
+
+      >
+
+        <Text>Tekan Disini</Text>
+
+      </TouchableOpacity>
+
+    </View>
+
+  );
+
+};
+
+ 
+
+const styles = StyleSheet.create({
+
+  container: {
+
+    flex: 1,
+
+    justifyContent: "center",
+
+    paddingHorizontal: 10
+
+  },
+
+  button: {
+
+    alignItems: "center",
+
+    backgroundColor: "#DDDDDD",
+
+    padding: 10
+
+  },
+
+  countContainer: {
+
+    alignItems: "center",
+
+    padding: 10
+
+  }
+
+});
+
+ export default App;
